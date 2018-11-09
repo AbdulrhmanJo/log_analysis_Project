9e328657cfa8219f9d357de381cee63eb587ba9b
Abdulrhman <44312747+AbdulrhmanJo@users.noreply.github.com>
2018-11-09 22:29:48 +0300

    some changes in style

--- news.py
+++ news.py
@@ -1,7 +1,10 @@
-import psycopg2 #import psycopg2 library
+#!/usr/bin/env python3
+
+import psycopg2
+
 
 def db_machine(query):
-    #this is method will excute all the query we add in this program
+    """this is method will excute all the query we add in this program"""
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(query)
@@ -9,16 +12,15 @@ def db_machine(query):
     db.close()
     return results
 
+
 def most_viewer_articles():
-    # here we collect the most Viewer articles
+    """here we collect the most Viewer articles"""
     mostViewer = db_machine("""
         select title,count(*) as number
         from articles,log
         where articles.slug = substr(log.path, 10)
         group by articles.title
         order by number desc limit 3; """)
-
-    # here will print the result of the query
     print("The most popular three articles:")
     i = 0
     while i < len(mostViewer):
@@ -28,16 +30,16 @@ def most_viewer_articles():
         print(number+title+views)
         i = i+1
 
+
 def most_popular_author():
-    # here we collect the most popular author
+    """here we collect the most popular author"""
     mostPopular = db_machine("""
         select authors.name, count(*) as number
         from authors,articles,log
-        where (authors.id = articles.author) and (articles.slug = substr(log.path, 10))
+        where (authors.id = articles.author) and
+        (articles.slug = substr(log.path, 10))
         group by authors.name
         order by number desc; """)
-
-    #print the result of this query
     print("\nThe most popular author:")
     i = 0
     while i < len(mostPopular):
@@ -47,26 +49,26 @@ def most_popular_author():
         print(number+author+views)
         i = i+1
 
+
 def requests_errors():
-    # check and collect the error request that ocurre in site
+    """check and collect the error that ocurre in site"""
     errors = db_machine("""
-        select total_status.date,round(((error_status.error * 100.0)/total_status.status), 1) as percent
+        select total_status.date,
+        round(((error_status.error * 100.0)/total_status.status), 1) as percent
         from error_status,total_status
         where error_status.date=total_status.date
         order by percent desc; """)
-        
-    #print the result of the query
     print("\nOn which days did more than 1% of requests lead to error: ")
     i = 0
     while i < len(errors):
-        if errors[i][1] >= 1: #check if the rate greater than 1 percent or not
+        if errors[i][1] >= 1:
             date = errors[0][0].strftime("%B %d,%Y")
             errorRate = ": "+str(errors[i][1])+"% errors"
             print(date+errorRate)
         i = i+1
 
+
 if __name__ == '__main__':
-    #main
     most_viewer_articles()
     most_popular_author()
     requests_errors()
